c4e6c562b07e4e56dca02371c7813427741cf9b6
HlaingMinThan <hlaingminthan92@gmail.com>
2020-11-29 13:21:18 +0630

    Github Top Nav

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  purge: [
+    './style.css'
+  ],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
